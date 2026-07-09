6112016e34c0179f87e22cc0dc97bc0a8e1daace
therjyer <sekimperium@gmail.com>
2026-07-09 03:08:54 -0300

    main

--- src/br/com/sistemabancario/ui/Main.java
+++ src/br/com/sistemabancario/ui/Main.java
@@ -1,17 +1,23 @@
 package br.com.sistemabancario.ui;
 
+import br.com.sistemabancario.gerenciamento.Banco;
 import br.com.sistemabancario.persistencia.LoggerSistema;
 
 /**
  * Ponto de entrada (Entry Point) principal da aplicação.
- * Mantém-se extremamente enxuto, limitando-se a inicializar a interface do utilizador.
+ * Mantém-se extremamente enxuto, limitando-se a inicializar os dados
+ * de persistência e a interface gráfica do sistema.
  */
 public class Main {
 
     public static void main(String[] args) {
+        // Registra o início da execução da aplicação no arquivo de log
         LoggerSistema.registrarLog("Inicializando o Sistema Bancário OOP...");
         
-        // Instancia e inicia o loop da interface gráfica do utilizador
+        // Inicializa os dados (recuperando as contas e clientes do arquivo CSV)
+        Banco.getInstancia().inicializarDados();
+        
+        // Instancia e inicia o loop da interface gráfica do utilizador (UI)
         MenuInterativo interfaceUsuario = new MenuInterativo();
         interfaceUsuario.iniciarLoop();
     }
